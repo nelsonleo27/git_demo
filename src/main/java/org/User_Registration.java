432fc5af08f67df42ff4fffc8dcd5e1b6b0dafec
nelson leo <200227leo27@gmail.com>
2025-04-16 18:09:15 +0530

    changes is made on user registration2

--- src/main/java/org/User_Registration.java
+++ src/main/java/org/User_Registration.java
@@ -2,10 +2,9 @@ package org;
 
 public class User_Registration {
     public static void main(String[] args) {
-
-
         System.out.println("input");
         System.out.println("p");
         System.out.println("rere");
+        System.out.println("gufs");
     }
 }
